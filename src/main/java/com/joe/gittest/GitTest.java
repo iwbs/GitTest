a400909c3c1bafaf08674dd626a9d26593b6997d
Joseph Yuen <joseph.yuen@redflaggroup.com>
2016-03-31 15:54:46 +0800

    Update

--- src/main/java/com/joe/gittest/GitTest.java
+++ src/main/java/com/joe/gittest/GitTest.java
@@ -3,7 +3,7 @@ package com.joe.gittest;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("This is master");
+		System.out.println("This is branch 1.0");
 	}
 
 }
